fc2f964e08190789bbca47a37f9202a26c086a05
dehwyy <dehwyy@yandex.ru>
2025-01-15 07:33:21 +0300

    [feat=`apps/stream_host`]: logger integration

--- apps/stream_host/src/main.rs
+++ apps/stream_host/src/main.rs
@@ -1,13 +1,16 @@
+use log::{error, info};
 mod srt_server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    log::Logger::with_config(log::LoggerConfig::new().sentry(false));
+
     let addr_cfg = config::new::<config::Addr>();
     let port = addr_cfg.ports().srt_server;
 
     match tokio::try_join!(srt_server::start(port),) {
-        Ok((_,)) => println!("All done!"),
-        Err(e) => eprintln!("Error: {}", e),
+        Ok((_,)) => info!("All done!"),
+        Err(e) => error!("Error: {}", e),
     }
 
     Ok(())
--- apps/stream_host/src/srt_server/mod.rs
+++ apps/stream_host/src/srt_server/mod.rs
@@ -1,4 +1,5 @@
 use futures::{StreamExt, TryStreamExt};
+use log::info;
 use srt_protocol::settings::KeySettings;
 use srt_tokio::{ConnectionRequest, SrtListener, SrtSocket};
 use tokio::net::{TcpListener, TcpStream};
@@ -18,14 +19,14 @@ async fn handle_srt_connection(conn: ConnectionRequest) {
     let key_settings: Option<KeySettings> = None;
 
     let remote_addr = conn.remote();
-    println!("New connection: {remote_addr}");
+    info!("New connection: {remote_addr}");
 
     let mut socket = conn.accept(key_settings).await.expect("Error accepting connection.");
 
     let mut buf: Vec<u8> = vec![];
     let mut i = 0;
     while let Ok(Some((timestamp, data))) = socket.try_next().await {
-        println!(
+        info!(
             "{remote_addr}: frame={i} timestamp={timestamp:?} datasize={} bufsize(kb)={}",
             data.len(),
             buf.len() / 1024
